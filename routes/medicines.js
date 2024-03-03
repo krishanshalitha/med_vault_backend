6a860a188f71ff95df94e8fa0bccee02894f265a
Krishan Shalitha <krishanshalitha@gmail.com>
2024-03-04 01:22:52 +0530

    Get all medicines

---
+++ routes/medicines.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    updateWorkout,
+    deleteWorkout
+}=require('../controllers/medicineController')
+
+const router= express.Router()
+
+// get all workout
+router.get('/',getWorkouts)
+
+//get single workout
+router.get('/:id', getWorkout)
+ 
+// POST a new workout
+router.post('/', createWorkout)
+
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+module.exports=router
---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express=require('express')
+const mongoose=require('mongoose')
+
+//express app
+const app=express()
+
+//use middleware
+app.use(express.json())
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+app.get('/', (req,res)=>{
+    res.json({mssg:'Welcome to the app'})
+})
+
+// routes
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        // listen request
+        app.listen(process.env.PORT, ()=>{
+            console.log('connect to db & listing on port', process.env.PORT)
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+process.env
