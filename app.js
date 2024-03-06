5332517ce65fd1e9362073044b79807b6e0074e4
Krishan Shalitha <krishanshalitha@gmail.com>
2024-03-06 23:06:34 +0530

    Test the server wether connect or not

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-/**
-* @description Globle library imports to the back-end
-* */
 const express = require('express'); //setup the globle libraries
 const app = express();
 const morgan = require('morgan'); //Logger for node
@@ -42,80 +39,4 @@ mongoose.connect(MONGODB_URI, option, (err) => {
    }
 });
 
-
-//Test the mongodb connection
-mongoose.connection.on('connected',() => {
-   console.log('Mongoose is connected');
-});
-
-
-//The connection throws an error
-mongoose.connection.on("error",function(err){
-   console.log('Mongoose default connection error: ' + err);
-});
-
-
-//connection disconnected
-mongoose.connection.on('disconnected',function(){
-   console.log('Mongoose default connection is disconnected');
-});
-
-
-//Close the mongoose connection
-process.on("SIGINT", function(){
-   mongoose.connection.close(function(){
-       console.log("Mongoose default connection disconnected through web application"
-       );
-       process.exit(0)
-   });
-
-
-})
-
-
-app.use(morgan('dev')); //Used Morgan
-//app.use('/uploads',express.static('uploads'));
-app.use(bodyParser.urlencoded({extended: false}));  //this for encorded Url's supporting rech data
-app.use(bodyParser.json());     //body paser for json data
-
-
-app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers",
-   "Origin, X-Requested-With, Contend-Type, Accept, Authorization"
-   );
-   if (req.method === 'OPTIONS') {
-       res.header('Access-Control-Allow-Methods', 'PUT , POST , PATCH , DELETE , GET');
-       return res.status(200).json({});
-   }
-   next();
-});
-
-
-
-
-
-
-app.use("/medicines", medicineRoutes);
-// app.use("/user", userRoutes);
-
-
-app.use((req, res, next) => {
-   const error = new Error('Not Found');
-   error.status = 404;
-   next(error);
-})
-
-
-app.use((error, req, res, next) => {
-   res.status(err.status || 500);
-   res.json({
-       error: {
-           message: error.message
-       }
-   })
-})
-
-
-module.exports = app;
 
--- controllers/medicineController.js
+++ controllers/medicineController.js
@@ -1,89 +1,154 @@
-const Medicine=require('../models/medicineModel')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect('mongodb://localhost:27017/your_database_name', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true // to avoid deprecation warning
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err)); // Handle error here
+
+// Routes
+const medicineRoutes = require('./routes/medicineRoutes');
+app.use('/medicines', medicineRoutes);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
+
+// const Medicine=require('../models/medicineModel')
+// const mongoose = require('mongoose')
+
+
+
+// //control method for add medicine details
+// exports.addNewMedicineDetails = (req, res, next) => {
+//     const medicine = new Medicine({
+//         _id:new mongoose.Types.ObjectId(),
+//         medname:req.body.medname,
+//         description:req.body.description,
+//         price:req.body.price,
+//         quantity:req.body.quantity,
+//           });
+//     medicine
+//         .save()
+//         .then(result => {
+//             console.log(result);
+//             res.status(200).json({
+//                 message: "Created medicine details successfully",
+//                 createdMedicine: {
+//                     _id:result._id,
+//                     medname:result.medname,
+//                     description:result.description,
+//                     price:result.price,
+//                     quantity:result.quantity,                   
+//                     request: {
+//                             type: 'GET',
+//                             url:'http://localhost:4000/medicines/' + result._id
+//                     }
+//                 }
+//         });
+//     })
+//     .catch(err => {
+//         console.log(err);
+//             res.status(501).json({
+//                 message: 'Can\'t create a new  player details, Please try again later'
+//         });
+//     });
+//  }
 
  
  
 
-//get all Medicines
-const getMedicines=async (req,res )=> {
-    const Medicines = await Medicine.find({}).sort({createdAt: -1})
-    res.status(200).json(Medicines)
-}
+// //get all Medicines
+// const getMedicines=async (req,res )=> {
+//     const Medicines = await Medicine.find({}).sort({createdAt: -1})
+//     res.status(200).json(Medicines)
+// }
 
 
-// get a single Medicine
-const getMedicine=async (req,res )=> {
-    const {id}= req.params
+// // get a single Medicine
+// const getMedicine=async (req,res )=> {
+//     const {id}= req.params
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such a Medicine'})
-    }
+//     if(!mongoose.Types.ObjectId.isValid(id)){
+//         return res.status(404).json({error: 'No such a Medicine'})
+//     }
 
-    const Medicine = await Medicine.findById(id)
+//     const Medicine = await Medicine.findById(id)
 
-    if(!Medicine){
-        return res.status(400).json({error: 'No such Medicine'})
-    }
-    res.status(200).json(Medicine)
-}
-
-
-//create new Medicine
-const createMedicine=async (req,res)=>{
-    const {title,load,reps}=req.body
-
-    // add doc to db
-    try{
-        const Medicine=await Medicine.create({medname,description,price,quantity})
-        res.status(200).json(Medicine)
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
-}
-
-// delete a Medicine
-const deleteMedicine= async (req,res)=>{
-    const {id}= req.params
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such a Medicine'})
-    }
-
-    const Medicine=await Medicine.findByIdAndDelete({_id: id})
-
-    if(!Medicine){
-        return res.status(400).json({error: 'No such Medicine'})
-    }
-
-    res.status(200).json(Medicine)
-}
-
-// update a Medicine
-const updateMedicine=async (req,res)=>{
-    const {id}= req.params
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such a Medicine'})
-    }
-
-    const Medicine = await Medicine.findOneAndUpdate({_id:id},{
-        ...req.body
-    })
-
-    if(!Medicine){
-        return res.status(400).json({error: 'No such Medicine'})
-    }
-
-    res.status(200).json(Medicine)
-}
+//     if(!Medicine){
+//         return res.status(400).json({error: 'No such Medicine'})
+//     }
+//     res.status(200).json(Medicine)
+// }
 
 
-module.exports={
-    getMedicines,
-    getMedicine,
-    createMedicine,
-    deleteMedicine,
-    updateMedicine
-}
+// //create new Medicine
+// const createMedicine=async (req,res)=>{
+//     const {title,load,reps}=req.body
+
+//     // add doc to db
+//     try{
+//         const Medicine=await Medicine.create({medname,description,price,quantity})
+//         res.status(200).json(Medicine)
+//     }catch(error){
+//         res.status(400).json({error: error.message})
+//     }
+// }
+
+// // delete a Medicine
+// const deleteMedicine= async (req,res)=>{
+//     const {id}= req.params
+
+//     if(!mongoose.Types.ObjectId.isValid(id)){
+//         return res.status(404).json({error: 'No such a Medicine'})
+//     }
+
+//     const Medicine=await Medicine.findByIdAndDelete({_id: id})
+
+//     if(!Medicine){
+//         return res.status(400).json({error: 'No such Medicine'})
+//     }
+
+//     res.status(200).json(Medicine)
+// }
+
+// // update a Medicine
+// const updateMedicine=async (req,res)=>{
+//     const {id}= req.params
+
+//     if(!mongoose.Types.ObjectId.isValid(id)){
+//         return res.status(404).json({error: 'No such a Medicine'})
+//     }
+
+//     const Medicine = await Medicine.findOneAndUpdate({_id:id},{
+//         ...req.body
+//     })
+
+//     if(!Medicine){
+//         return res.status(400).json({error: 'No such Medicine'})
+//     }
+
+//     res.status(200).json(Medicine)
+// }
+
+
+// module.exports={
+//     getMedicines,
+//     getMedicine,
+//     createMedicine,
+//     deleteMedicine,
+//     updateMedicine
+// }
